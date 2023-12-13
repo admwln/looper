9ccc1926f2aa91a6f2e83cb9eee057abdcf4a556
Adam Welin <welinadam@users.noreply.github.com>
2023-12-13 09:14:13 +0100

    createSequence() function working, and using Tone.Draw

--- tone-loop/index.js
+++ tone-loop/index.js
@@ -2,11 +2,16 @@
 let kick;
 let snare;
 let lowPass;
-let snareLoop;
-let kickLoop;
-let index = 0;
-const notes = [null, "A2", null, "F2", null, "D2", null, "F2"];
-const snareSeq = [0, 1, 0, 1, 0, 1, 0, 1];
+let snareSequence;
+let kickSequence;
+let hihatSequence;
+
+const notes = [];
+const kickNoteValues = ["C4", "C5", "C6", "C4", "C1", "C1", "C1", "C1"];
+
+const snareSeq = [0, 0, 1, 0, 0, 0, 1, 0];
+const kickSeq = [1, 0, 0, 0, 1, 1, 0, 0];
+const hihatSeq = [1, 1, 1, 1, 1, 1, 1, 1];
 
 document.querySelector("#tone-js-init").addEventListener("click", async () => {
   await Tone.start();
@@ -46,8 +51,8 @@ document.querySelector("#tone-js-init").addEventListener("click", async () => {
     },
     envelope: {
       attack: 0.001,
-      decay: 0.1,
-      sustain: 0.1,
+      decay: 0.015,
+      sustain: 0.015,
       release: 0.02,
     },
   })
@@ -55,36 +60,51 @@ document.querySelector("#tone-js-init").addEventListener("click", async () => {
     .toDestination();
 
   // BPM
-  Tone.Transport.bpm.value = 120;
+  Tone.Transport.bpm.value = 90;
 
-  // SNARE LOOP
-  snareLoop = new Tone.Loop((time) => {
-    if (snareSeq[index] === 1) {
-      snare.triggerAttackRelease(/*notes[index], */ "4n", time);
-      drawBlock();
-      console.log(time);
-    }
-    index++;
-
-    // Reset index when it reaches the end of the array
-    if (index === notes.length) {
-      index = 0;
-    }
-  }, "4n");
-
-  // KICK LOOP
+  // SEQS
+  snareSequence = createSequence(snare, snareSeq, notes);
+  kickSequence = createSequence(kick, kickSeq, kickNoteValues);
+  hihatSequence = createSequence(hihat, hihatSeq, notes);
 });
+
+function createSequence(instrument, sequence, noteValues) {
+  let index = 0;
+  return new Tone.Sequence(
+    (time, note) => {
+      if (note === 1) {
+        let noteValue = noteValues[index];
+        if (instrument === kick) {
+          instrument.triggerAttackRelease(noteValue, "8n", time);
+        } else {
+          instrument.triggerAttackRelease("8n", time);
+        }
+
+        Tone.Draw.schedule(() => {
+          drawBlock();
+        }, time);
+        index++;
+        if (index === noteValues.length) {
+          index = 0;
+        }
+      }
+    },
+    sequence,
+    "8n"
+  );
+}
 
 const play = document.querySelector("#play");
 play.addEventListener("click", () => {
   Tone.Transport.start();
-  snareLoop.start(0);
+  snareSequence.start(0);
+  kickSequence.start(0);
+  hihatSequence.start(0);
 });
 
 const stop = document.querySelector("#stop");
 stop.addEventListener("click", () => {
   Tone.Transport.stop();
-  snareLoop.stop();
 });
 
 function drawBlock() {
