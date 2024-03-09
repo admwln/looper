fad5212a201afdda8f34a1dcecc9f2f88796162c
Adam Welin <adamwelin@gmail.com>
2024-03-09 13:44:49 +0100

    Move relevant methods from Keyboard to Chord class

--- js/Chord.js
+++ js/Chord.js
@@ -1,4 +1,10 @@
-import { getKeyboard, setIdCounter, getIdCounter } from "./helper-functions.js";
+import {
+  getCurrentChord,
+  getKeyboard,
+  setIdCounter,
+  getIdCounter,
+  getProject,
+} from "./helper-functions.js";
 
 export default class Chord {
   constructor(name) {
@@ -44,5 +50,64 @@ export default class Chord {
       }
       $(`#${key.id} .key-no`).text("");
     }
+  }
+
+  save() {
+    // Method is called on the current chord, this = current chord
+    const chordName = prompt("Enter chord name");
+    const chord = new Chord(chordName);
+    const currentChordNotes = [...this.notes]; // Using spread operator for shallow copy
+    currentChordNotes.forEach((note) => {
+      chord.notes.push(note);
+    });
+    getProject().chords.push(chord);
+    getKeyboard().updateChordList();
+    chord.selectButton();
+    // Update current chord id and name
+    this.id = chord.id;
+    this.name = chord.name;
+  }
+
+  select() {
+    getKeyboard().clear();
+    // Set current chord to selected chord
+    const currentChord = getCurrentChord();
+    currentChord.notes = [];
+    // Copy of selected chord notes
+    const selectedChordNotes = [...this.notes];
+    selectedChordNotes.forEach((note) => {
+      currentChord.notes.push(note);
+    });
+    // Update current chord id and name
+    currentChord.name = this.name;
+    currentChord.id = this.id;
+    // Update keyboard
+    getKeyboard().keys.forEach((key) => {
+      if (currentChord.notes.includes(key.midiNote)) {
+        key.on = true;
+        $(`#${key.id}`).addClass("on");
+      }
+    });
+    currentChord.updateKeyNos();
+  }
+
+  selectButton() {
+    $(`#${this.id}`).addClass("selected");
+    $(`#${this.id}`).siblings().removeClass("selected");
+  }
+
+  delete() {
+    // Method is called on the current chord, this = current chord
+    getKeyboard().clear();
+    const chordId = this.id;
+    const chords = getProject().chords;
+    // Find chord in project that corresponds to current chord, using chordId
+    const index = chords.findIndex((chord) => chord.id === chordId);
+    chords.splice(index, 1);
+    getKeyboard().updateChordList();
+    // Remove all notes from current chord, rename to "Current"
+    this.notes = [];
+    this.name = "Current";
+    this.updateKeyNos(); // Clear key-nos
   }
 }
